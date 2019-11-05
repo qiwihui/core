ecd1c178b1e14f882c7ed8f06a29081c9a6dabcb
qiwihui <qwh005007@gmail.com>
2019-11-05 18:51:59 +0800

    add hans

---
+++ Formula/hans.rb
@@ -0,0 +1,17 @@
+class Iodine < Formula
+    desc "Tunnel IP over ICMP"
+    homepage "http://code.gerade.org/hans/"
+    url "https://github.com/friedrich/hans/archive/v1.0.tar.gz"
+    version "1.0"
+    sha256 "53090083d440466e573b35f2eeab0b4b0dcd3e6290f797c999b4f5a0b5caaba2"
+
+    def install
+      system "make"
+      bin.install "bin/hans"
+    end
+  
+    test do
+      system "#{sbin}/hans", "-v"
+    end
+  end
+  
